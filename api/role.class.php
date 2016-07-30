984c03d964e84b814c61048ad048f1c4da62dd2d
gnaw10 <gnaw10@qq.com>
2016-07-30 11:11:09 +0800

    1.4

--- api/role.class.php
+++ api/role.class.php
@@ -11,9 +11,7 @@ class Role
 		
 		$sqlRole=$pdo->prepare('SELECT * FROM `role` WHERE `rid` = :roleId;');
 		$sqlRole->bindValue(':roleId',(int)$roleId,PDO::PARAM_INT);
-		$sqlRole->execute();
-		
-		
+		$sqlRole->execute();	
 
 		if( ($response=$sqlRole->fetch(PDO::FETCH_ASSOC))==false )
 		{
@@ -21,9 +19,36 @@ class Role
 		}
 		else 
 		{
-			
 			return $response['name'];
 		}
+	}
+
+	public function list()
+	{
+		global $pdo;
+		$sqlRole=$pdo->prepare('SELECT * FROM `role`;');
+		$sqlRole->execute();
+		if( ($response=$sqlRole->fetchall(PDO::FETCH_ASSOC))==false)
+		{
+			return false;
+		}
+		else 
+		{
+			foreach($response as &$i)
+			{
+				$i['name']=urldecode($i['name']);
+			}
+			return $response;
+		}
+	}
+
+	public function modify($rid,$rName)
+	{
+		global $pdo;
+		$sqlRole=$pdo->prepare('UPDATE `role` SET `name` = :rName WHERE `rid` = :rid;');
+		$sqlRole->bindValue(':rName',urlencode($rName),PDO::PARAM_STR);
+		$sqlRole->bindValue(':rid',(int)($rid),PDO::PARAM_INT);
+		return $sqlRole->execute();
 	}
 }
 
---
+++ api/role.request.php
@@ -0,0 +1,47 @@
+<?php
+	require_once('role.class.php');
+	require_once('user.class.php');
+	require_once('site.class.php');
+	
+	if($action[1]=='list')
+	{
+		$response=Role::list();
+		if($response==false)
+		{
+			handle(ERROR_SYSTEM.'00');
+		}
+		else 
+		{
+			handle('0000'.json_encode($response));
+		}
+	}
+
+	if($action[1]=='renew')
+	{
+		$nowUser=User::show(Site::getSessionUid());
+		$nowRoleId=$nowUser[0]['roleId'];
+		if(getRequest('rid')<=4&&getRequest('rid')>=1)
+		{
+				if($nowRoleId==1)
+			{
+				$response = Role::modify(getRequest('rid'),getRequest('name'));
+				if($response==false)
+				{
+					handle(ERROR_SYSTEM.'03');
+				}
+				else 
+				{
+					handle('0000');
+				}
+			}
+			else 
+			{
+				handle(ERROR_PERMISSION.'03');
+			}
+		}
+		else 
+		{
+			handle(ERROR_INPUT.'03');
+		}
+}
+?>
