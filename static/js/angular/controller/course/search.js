e00714961d67a0133dd6b24bd3c48991e6fe1796
qk2 <guessever@gmail.com>
2016-08-07 11:23:05 +0800

    course list

--- static/js/angular/controller/course/search.js
+++ static/js/angular/controller/course/search.js
@@ -1,2 +1,22 @@
-app.controller('course.search', ['$scope', '$rootScope', '$http', '$state', function($scope, $rootScope, $http, $state){
+app.controller('course.search', ['$scope', '$rootScope', '$http', '$state', '$uibModal', '$timeout', function($scope, $rootScope, $http, $state, $uibModal, $timeout){
+	var searchKey = {
+		ownerId: $state.params.ownerId,
+		name: $state.params.name
+	}
+	$scope.courses = [];
+	var fetchCourseList = function () {
+		$http.post('/api/course/list', searchKey).success(function (response) {
+			if(response.code === '0000') {
+				$scope.courses = response.response;
+			}
+		});
+	}
+	$timeout(fetchCourseList, 0);
+
+	$scope.addCourse = function () {
+		$uibModal.open({
+			templateUrl: '/template/course/add.html',
+			controller: 'course.add'
+		});
+	}
 }]);
